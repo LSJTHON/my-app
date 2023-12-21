464c846ede65c68c8bf0ac6cb73a78344c932895
Lee Seong Jong <dltjdwhd1235@naver.com>
2023-12-21 11:59:31 +0900

    React 실습1

---
+++ src/chapter_03/Book.jsx
@@ -0,0 +1,34 @@
+//리액트 함수 컴포넌트 .jsx
+import React from "react";
+
+//jsx 버전
+// function Book(props) {
+//   return (
+//     <div>
+//       <h1>{`이 책의 이름은 ${props.name} 입니다.`}</h1>
+//       <h2>{`이 책은 총 ${props.numOfPage} 페이지로 이뤄져 있습니다.`}</h2>
+//     </div>
+//   );
+// }
+
+function Book(props){
+  return React.createElement(
+    'div',
+    null,
+    [
+        React.createElement(
+          'h1',
+          null,
+          `이 책의 이름은 ${props.name}입니다.`
+        ),
+        React.createElement(
+          'h2',
+          null,
+          `이 책은 총 ${props.numOfPage} 페이지로 이루어져 있습니다.`
+        )
+    ]
+  )
+}
+
+
+export default Book; //내보내기
