93670bd019f09fb00358e93790b21071d90683bd
Lee Seong Jong <dltjdwhd1235@naver.com>
2023-12-26 14:03:20 +0900

    소개글

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+
+function ProfileCard(props) {
+  return (
+    <Card title="SeongJong Lee" backgroundColor="#4ea04e">
+      <p>안녕하세요 누구세요?</p>
+      <p>저는 chatGPT로 꿀빨고 있습니다.</p>
+    </Card>
+  );
+}
+
+export default ProfileCard;
